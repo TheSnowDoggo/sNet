0f991214a1102bae4b8c3a9449f11bf693241433
Luna Bl3p <redstonewolfer@Gmail.com>
2026-03-08 17:01:35 +0000

    merged sNetServer

---
+++ Server/src/Program.cs
@@ -0,0 +1,28 @@
+﻿using sNet;
+
+namespace sNetServer;
+
+internal static class Program
+{
+	private static void Main()
+	{
+		Logger.Initialize();
+
+		try
+		{
+			if (!new Server().Run())
+			{
+				Console.Read();
+			}
+		}
+		catch (Exception ex)
+		{
+			Logger.Error(ex.Message);
+			Console.Read();
+		}
+		finally
+		{
+			Logger.Close();
+		}
+	}
+}
---
+++ Server/src/Server.cs
@@ -0,0 +1,161 @@
+﻿using SCENeo;
+using sNet;
+using sNet.Auth;
+using sNet.CScriptPro;
+using sNet.Server;
+using sNet.Service.Asset;
+using sNet.Service.Chat;
+using sNet.Service.Cmd;
+using sNet.Service.Part;
+
+namespace sNetServer;
+
+public sealed class Server
+{
+	private const string ServerDirectory = "server";
+	
+	private readonly Updater _updater;
+	private Thread _updateThread;
+	
+	private NetServer _server;
+	private readonly ServerPartRoot _root = new ServerPartRoot();
+	private readonly ServerPartService _partService = new ServerPartService();
+	private readonly ServerAssetService _assetService = new ServerAssetService();
+	private readonly ServerCmdService _cmdService = new ServerCmdService();
+
+	public Server()
+	{
+		_updater = new Updater(Update)
+		{
+			FrameCap = 60,
+		};
+		
+		_root.Service = _partService;
+		_partService.Root = _root;
+	}
+	
+	public bool Run()
+	{
+		PackageManager.Default.Packages.Add("Server", new ServerPackage());
+		
+		if (!ServerConfig.TryLoadOrCreate("server_config.json", out var config))
+		{
+			return false;
+		}
+
+		_server = NetServer.FromConfig(config);
+		
+		_server.Services.Add(new ServerChatService());
+		_server.Services.Add(_partService);
+		_server.Services.Add(_assetService);
+		_server.Services.Add(_cmdService);
+		
+		_server.ClientJoined += Server_OnClientJoined;
+		_server.ClientLeft += Server_OnClientLeft;
+		
+		if (!_server.Bind())
+		{
+			return false;
+		}
+		
+		Logger.Info($"Server started at {_server.EndPointName}");
+
+		if (!_server.Start())
+		{
+			return false;
+		}
+
+		if (!UserStore.TryLoadOrCreate("users.json", out var users))
+		{
+			return false;
+		}
+
+		UserStore.Current = users;
+
+		Directory.CreateDirectory("assets");
+
+		if (!AssetIndex.TryDiscover("assets", out var index))
+		{
+			return false;
+		}
+		
+		_assetService.Index = index;
+		
+		Logger.Info($"Discovered {index.Assets.Length} assets.");
+
+		LoadServerParts();
+
+		Script.RunScripts(_root.Root, ScriptFlag.Server);
+
+		_updateThread = new Thread(_updater.Start);
+		_updateThread.Start();
+
+		while (_server.Active)
+		{
+			var input = Console.ReadLine();
+
+			if (string.IsNullOrWhiteSpace(input))
+			{
+				continue;
+			}
+
+			_cmdService.TryRun(input);
+		}
+		
+		return true;
+	}
+
+	private static void Server_OnClientJoined(RemoteClient client)
+	{
+		ServerPackage.PlayerJoin.Fire(client.Idx);
+	}
+	
+	private static void Server_OnClientLeft(RemoteClient client)
+	{
+		ServerPackage.PlayerQuit.Fire(client.Idx);
+	}
+
+	private void Update(double delta)
+	{
+		if (!_server.Active)
+		{
+			_updater.Stop();
+			return;
+		}
+		
+		_root.Update(delta);
+	}
+
+	private void LoadServerParts()
+	{
+		Directory.CreateDirectory(ServerDirectory);
+
+		int parts = 0;
+		
+		foreach (var file in FileUtils.EnumerateFilesRecursive(ServerDirectory))
+		{
+			if (Path.GetExtension(file) != ".part")
+			{
+				continue;
+			}
+			
+			try
+			{
+				var tags = PartTag.ParseAll(file);
+
+				foreach (var tag in tags)
+				{
+					_root.Root.AddChild(tag.Create());
+
+					parts++;
+				}
+			}
+			catch (Exception ex)
+			{
+				Logger.Error($"Failed to load parts from {file}: {ex.Message}");
+			}
+		}
+		
+		Logger.Info($"Successfully loaded {parts} parts.");
+	}
+}
